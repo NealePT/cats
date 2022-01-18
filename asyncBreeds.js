15638cd95299b8fc208f867270d0a879e4d15497
Neale Taylor <nealeptaylor@gmail.com>
2022-01-18 18:18:47 -0500

    implemented mocha testing for both files

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -4,6 +4,7 @@ const breedDetailsFromFile = function(breed, functionToRunWhenDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
+    if (error) functionToRunWhenDone(undefined);
     if (!error) functionToRunWhenDone(data);
   });
 };
@@ -12,4 +13,5 @@ const printCatBreed = breed => {
   console.log('Return Value: ', breed);
 };
 
-breedDetailsFromFile('Bombay', printCatBreed);
+module.exports = breedDetailsFromFile;
+
